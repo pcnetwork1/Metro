a3e0540bf51b95dad733859517205e5c6f2f6432
Ijozing <ivan.jozing1@gmail.com>
2019-11-29 11:19:06 +0100

    Update 1.0

--- script.sql
+++ script.sql
@@ -15,8 +15,9 @@ create table racun(
     stavka int not null,
     kupac int not null,
     vrijemekupnje datetime,
-    nacinplacanja int not null,
-    djelatnik int not null
+    nacinplacanja varchar(20),
+    djelatnik int not null,
+    brojracuna varchar(50)
 );
 
 create table stavka(
@@ -28,15 +29,10 @@ create table proizvod(
     id int primary key auto_increment,
     naziv varchar(50),
     barcode char(20),
-    kategorija int not null,
+    kategorija varchar(20),
     racun int not null
 );
 
-create table kategorija(
-    id int primary key auto_increment,
-    naziv varchar(20),
-    lokacija varchar(20)
-);
 
 create table djelatnik(
     id int primary key auto_increment,
@@ -44,27 +40,19 @@ create table djelatnik(
     prezime varchar(20) not null,
     iban varchar(15),
     brojmobitela char(15),
-    menadzer int not null
+    menadzer boolean 
 
 );
 
-create table menadzer(
-    id int primary key auto_increment,
-    ime varchar(20),
-    prezime varchar(20),
-    brojmobitela char(20)
-);
-
-create table nacinplacanja(
-    id int primary key auto_increment,
-    naziv varchar(20)
-);
 
 
 alter table racun add foreign key (kupac) references kupac(id);
 alter table racun add foreign key (djelatnik) references djelatnik(id);
-alter table racun add foreign key (nacinplacanja) references nacinplacanja(id);
 alter table stavka add foreign key (racun) references racun(id);
 alter table stavka add foreign key (proizvod) references proizvod(id);
-alter table proizvod add foreign key (kategorija) references kategorija(id);
-alter table djelatnik add foreign key (menadzer) references menadzer(id);
+
+
+
+
+
+
